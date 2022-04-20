18c2c2362d91d8ec9d6d8414cd22150627c33c5a
Carlos Martinez <carlosm28@outlook.com>
2022-04-20 02:22:02 -0500

    Timeout for SharePoint File Increased

--- libraries/sharepoint/sharepoint.py
+++ libraries/sharepoint/sharepoint.py
@@ -18,13 +18,11 @@ class SharePoint():
         """
         try:
             log_message("Start - Download file from SharePoint")
-            self.browser.go_to(self.sharepoint_url)       
+            self.browser.go_to(self.sharepoint_url)
             file_download_url = act_on_element('//form[@id="office_form"]/input[@name="fileGetUrl"]', "find_element").get_attribute("value")
             self.browser.go_to(file_download_url)
-            check_file_download_complete("xlsx", 10)
+            check_file_download_complete("xlsx", 20)
             log_message("Finish - Download file from SharePoint")
         except Exception as e:
             capture_page_screenshot(OUTPUT_FOLDER, "Exception_Sharepoint_Download_File")
             raise Exception("Download file from SharePoint failed.")
-
-    
